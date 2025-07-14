07e28dc647d017feb896458d27cba9b36bb8b1a4
SerAbinU1 <abinyt1234@gmail.com>
2025-07-14 18:33:13 +0000

    Exercise 10.8: the sign-in form done

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,7 +1,71 @@
-import Text from "./Text";
+import { TextInput, View, Pressable, StyleSheet } from "react-native"
+import { useFormik } from "formik"
+
+import Text from "./Text"
+import theme from "../theme"
+
+const initialValues = {
+  username: "",
+  password: "",
+}
+
+const onSubmit = (values) => {
+  console.log(values)
+}
 
 const SignIn = () => {
-  return <Text>The sign-in view</Text>
+  const formik = useFormik({
+    initialValues,
+    onSubmit,
+  })
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        placeholder="username"
+        value={formik.values.username}
+        onChangeText={formik.handleChange("username")}
+        style={styles.input}
+      />
+      <TextInput
+        placeholder="password"
+        value={formik.values.password}
+        onChangeText={formik.handleChange("password")}
+        secureTextEntry
+        style={styles.input}
+      />
+      <Pressable onPress={formik.handleSubmit} style={styles.button}>
+        <Text fontWeight={"bold"} style={styles.buttonText}>
+          Submit
+        </Text>
+      </Pressable>
+    </View>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 20,
+    backgroundColor: theme.colors.mainBakground,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#000000",
+    padding: 10,
+    borderRadius: 4,
+    marginBottom: 10,
+  },
+  button: {
+    padding: 15,
+    borderRadius: 4,
+    backgroundColor: "blue",
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "white",
+  },
+})
 
 export default SignIn
